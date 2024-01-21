da0ed89a73dac0bd512e7b145d68e24f7730f458
clearly mine <clearlymine@pm.me>
2024-01-22 04:41:07 +0530

    [nvim] update ufo plugin

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -35,11 +35,19 @@ return {
       vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
       vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
 
-      require('ufo').setup {
-        provider_selector = function(_, _, _)
-          return { 'treesitter', 'indent' }
-        end,
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities.textDocument.foldingRange = {
+          dynamicRegistration = false,
+          lineFoldingOnly = true
       }
+      local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
+      for _, ls in ipairs(language_servers) do
+          require('lspconfig')[ls].setup({
+              capabilities = capabilities
+              -- you can add other fields for setting up lsp server in this table
+          })
+      end
+      require('ufo').setup()
     end,
   },
 
