dcb452841a3d0a08cfe5875efe16b8ce3b7ce5fc
clearly mine <clearlymine@pm.me>
2024-03-26 12:31:10 +0530

    [nvim] add package version info in package.json

--- nvim/lua/plugins/javascript.lua
+++ nvim/lua/plugins/javascript.lua
@@ -49,4 +49,12 @@ return {
     end,
   },
 
+  -- Package version checking
+  {
+    'vuki656/package-info.nvim',
+    dependencies = { 'MunifTanjim/nui.nvim' },
+    config = function()
+      require('package-info').setup()
+    end,
+  },
 }
