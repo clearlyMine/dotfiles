78253d6ed92363324ed6d842d44ecfb4023f82ae
clearly mine <clearlymine@pm.me>
2024-04-15 21:14:03 +0530

    [nvim] change mappings for autocomplete

--- nvim/lua/plugins/autocomplete.lua
+++ nvim/lua/plugins/autocomplete.lua
@@ -98,10 +98,10 @@ return {
           -- }),
           ['<C-n>'] = cmp.mapping.select_next_item(),
           ['<C-p>'] = cmp.mapping.select_prev_item(),
-          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-k>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-j>'] = cmp.mapping.scroll_docs(4),
           ['<C-Space>'] = cmp.mapping.complete {},
-          ['<CR>'] = cmp.mapping.confirm {
+          ['<C-f>'] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
           },
