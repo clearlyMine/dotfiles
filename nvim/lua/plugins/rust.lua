f553c8c43e540f902bbdead3b58d628643e5d275
clearly mine <clearlymine@pm.me>
2024-05-08 13:05:21 +0530

    [nvim] fix: rust's treesitter config
    The previous config meant that nothing would be extended upon if opts.ensure_installed is unpopulated

--- nvim/lua/plugins/rust.lua
+++ nvim/lua/plugins/rust.lua
@@ -107,7 +107,8 @@ return {
   {
     'nvim-treesitter/nvim-treesitter',
     opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed or {}, { 'ron', 'rust', 'toml' })
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { 'ron', 'rust', 'toml' })
     end,
   },
 
