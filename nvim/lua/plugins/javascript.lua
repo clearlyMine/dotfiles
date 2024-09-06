e3e8d20cce0cb69d325669d9e564cca1445c01d8
clearly mine <clearlymine@pm.me>
2024-09-06 14:16:57 +0530

    [nvim] ♻️ Standardize code formatting

--- nvim/lua/plugins/javascript.lua
+++ nvim/lua/plugins/javascript.lua
@@ -36,12 +36,12 @@ return {
       { '<leader>sv', '<cmd>TWValues<cr>', desc = 'Show tailwind CSS values' },
     },
     opts = {
-      border = 'rounded', -- Valid window border style,
+      border = 'rounded',          -- Valid window border style,
       show_unknown_classes = true, -- Shows the unknown classes popup
-      focus_preview = true, -- Sets the preview as the current window
-      copy_register = '', -- The register to copy values to,
+      focus_preview = true,        -- Sets the preview as the current window
+      copy_register = '',          -- The register to copy values to,
       keymaps = {
-        copy = '<C-y>', -- Normal mode keymap to copy the CSS values between {}
+        copy = '<C-y>',            -- Normal mode keymap to copy the CSS values between {}
       },
     },
   },
