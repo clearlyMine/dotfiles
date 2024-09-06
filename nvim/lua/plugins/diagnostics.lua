3fe21847f4077dad8b4b8dc86528618c4790b1bf
clearly mine <clearlymine@pm.me>
2024-09-06 14:20:06 +0530

    [nvim] 👽️ Updated to reflect latest config for trouble

--- nvim/lua/plugins/diagnostics.lua
+++ nvim/lua/plugins/diagnostics.lua
@@ -6,40 +6,12 @@ return {
     cmd = { 'TroubleToggle', 'Trouble' },
     opts = { use_diagnostic_signs = true },
     keys = {
-      { '<leader>xc', '<Cmd>TroubleClose<CR>',                        desc = 'Trouble Close' },
-      { '<leader>xx', '<Cmd>TroubleToggle document_diagnostics<CR>',  desc = 'Document Diagnostics (Trouble)' },
-      { '<leader>xw', '<Cmd>TroubleToggle workspace_diagnostics<CR>', desc = 'Workspace Diagnostics (Trouble)' },
-      { '<leader>xr', '<Cmd>TroubleToggle lsp_references<CR>',        desc = 'LSP References (Trouble)' },
-      { '<leader>xq', '<Cmd>TroubleToggle quickfix<CR>',              desc = 'Quickfix List (Trouble)' },
-      { '<leader>xl', '<Cmd>TroubleToggle loclist<CR>',               desc = 'Location List (Trouble)' },
-      {
-        '[q',
-        function()
-          if require('trouble').is_open() then
-            require('trouble').previous { skip_groups = true, jump = true }
-          else
-            local ok, err = pcall(vim.cmd.cprev)
-            if not ok then
-              vim.notify(err, vim.log.levels.ERROR)
-            end
-          end
-        end,
-        desc = 'Previous trouble/quickfix item',
-      },
-      {
-        ']q',
-        function()
-          if require('trouble').is_open() then
-            require('trouble').next { skip_groups = true, jump = true }
-            -- else
-            --   local ok, err = pcall(vim.cmd.cnext)
-            --   -- if not ok then
-            --   vim.notify(err, vim.log.levels.ERROR)
-            -- end
-          end
-        end,
-        desc = 'Next trouble/quickfix item',
-      },
+      { '<leader>xx', '<cmd>Trouble diagnostics toggle<cr>',                        desc = 'Diagnostics (Trouble)' },
+      { '<leader>xX', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',           desc = 'Buffer Diagnostics (Trouble)' },
+      { '<leader>cs', '<cmd>Trouble symbols toggle focus=false<cr>',                desc = 'Symbols (Trouble)' },
+      { '<leader>xr', '<cmd>Trouble lsp toggle focus=false win.position=right<cr>', desc = 'LSP Definitions / references / ... (Trouble)' },
+      { '<leader>xL', '<cmd>Trouble loclist toggle<cr>',                            desc = 'Location List (Trouble)' },
+      { '<leader>xQ', '<cmd>Trouble qflist toggle<cr>',                             desc = 'Quickfix List (Trouble)' },
     },
   },
 
